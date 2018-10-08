45a3a3cdcf7a120af0fd43fd2f90c19fbe4d742a
a-styuf <a-styuf@yandex.ru>
2018-10-08 18:22:45 +0700

    добавил комментарий

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
+        #
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         #
